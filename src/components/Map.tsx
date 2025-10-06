e0bee055bafdcda42eafa06f5666cf02a40b65c7
Juho Nuottimäki <67386247+jauhoja@users.noreply.github.com>
2025-10-06 12:38:49 +0300

    move styles to their own file

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -6,88 +6,9 @@ import {
   LineLayerSpecification,
   StyleSpecification,
 } from '@maplibre/maplibre-gl-style-spec';
-import {GeoJSON} from 'geojson';
 
-import * as defaultMapStyle from '../map-styles/style.json';
 import {Button} from 'react-native';
-
-let defaultStyle: StyleSpecification = defaultMapStyle as StyleSpecification;
-
-// in this example search origin is set to Hervanta
-const herwoodCenteredShipSource: GeoJSONSourceSpecification = {
-  type: 'geojson',
-  data: 'https://meri.digitraffic.fi/api/ais/v1/locations?radius=100&latitude=61.4481&longitude=23.8521',
-};
-
-const shipLayer: CircleLayerSpecification = {
-  id: 'ships',
-  type: 'circle',
-  source: 'fintraffic-ships',
-  paint: {
-    'circle-color': '#fa0',
-  },
-};
-
-const herwoodToCapetown: GeoJSON = {
-  type: 'FeatureCollection',
-  features: [
-    {
-      type: 'Feature',
-      geometry: {
-        type: 'LineString',
-        coordinates: [
-          [23.8521, 61.4481],
-          [18.4233, -33.918861],
-        ],
-      },
-      properties: {name: 'Hervanta - Cape Town'},
-    },
-  ],
-};
-
-const herwood2CapetownSource: GeoJSONSourceSpecification = {
-  type: 'geojson',
-  data: herwoodToCapetown,
-};
-
-const herwoodLayer: LineLayerSpecification = {
-  id: 'herwood-to-capetown',
-  type: 'line',
-  source: 'herwood-to-capetown',
-  paint: {
-    'line-cap': 'round',
-    'line-width': 10,
-    'line-color': '#fa0',
-  },
-};
-
-const plainPointSource: GeoJSONSourceSpecification = {
-  type: 'geojson',
-  data: {
-    type: 'FeatureCollection',
-    features: [
-      {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [19.93481, 60.09726],
-        },
-        properties: {name: 'Mariehamn'},
-      },
-    ],
-  },
-};
-
-const plainPointLayer: CircleLayerSpecification = {
-  id: 'plain-point',
-  type: 'circle',
-  source: 'plain-point',
-  paint: {
-    'circle-color': '#0a0',
-    'circle-stroke-width': 2,
-    'circle-stroke-color': '#000',
-  },
-};
+import {addHerwood2CapeLayer, addPointLayer, defaultStyle} from '../map-styles/styles';
 
 const cameraInitState = {
   centerCoordinate: [19.93481, 60.09726],
@@ -100,27 +21,12 @@ const Map = () => {
   const [camera, setCamera] = React.useState<any>(cameraInitState);
   const cameraref = React.useRef<Camera>(null);
 
-  const addLineLayer = () => {
-    setMapStyle({
-      ...mapStyle,
-      sources: {
-        ...mapStyle.sources,
-        'herwood-to-capetown': herwood2CapetownSource,
-      },
-      layers: [...mapStyle.layers, herwoodLayer],
-    });
+  const addHerwoodToCape = () => {
+    setMapStyle(addHerwood2CapeLayer(mapStyle));
   };
 
-  const addLayer = () => {
-    setMapStyle({
-      ...mapStyle,
-      sources: {
-        ...mapStyle.sources,
-        'fintraffic-ships': herwoodCenteredShipSource,
-        'plain-point': plainPointSource,
-      },
-      layers: [...mapStyle.layers, shipLayer, plainPointLayer],
-    });
+  const addPoints = () => {
+    setMapStyle(addPointLayer(mapStyle));
   };
 
   // set new center focus, defaults to mariehamn
@@ -151,8 +57,8 @@ if (defaultStyle) {
   return (
     <>
       <Button title="Reset" onPress={resetCamera}></Button>
-      <Button title="Show" onPress={addLineLayer}></Button>
-      <Button title="Show2" onPress={addLayer}></Button>
+      <Button title="Show" onPress={addHerwoodToCape}></Button>
+      <Button title="Show2" onPress={addPoints}></Button>
       <MapView id={'asdf'} style={{flex: 1}} mapStyle={mapStyle}>
         <Camera
           centerCoordinate={camera.centerCoordinate}
---
+++ src/map-styles/styles.ts
@@ -0,0 +1,113 @@
+import {
+  CircleLayerSpecification,
+  GeoJSONSourceSpecification,
+  SourceSpecification,
+  StyleSpecification,
+} from '@maplibre/maplibre-gl-style-spec';
+import * as defaultMapStyle from '../map-styles/style.json';
+
+export const defaultStyle: StyleSpecification =
+  defaultMapStyle as StyleSpecification;
+
+export const addHerwood2CapeLayer = (
+  prevStyle: StyleSpecification,
+): StyleSpecification => {
+  return {
+    ...prevStyle,
+    sources: {
+      ...prevStyle.sources,
+      'herwood-to-capetown': herwood2CapetownSource,
+    },
+    layers: [...prevStyle.layers, herwoodLayer],
+  };
+};
+
+export const addPointLayer = (
+  prevStyle: StyleSpecification,
+): StyleSpecification => {
+  return {
+    ...prevStyle,
+    sources: {
+      ...prevStyle.sources,
+      'fintraffic-ships': herwoodCenteredShipSource,
+      'plain-point': plainPointSource,
+    },
+    layers: [...prevStyle.layers, shipLayer, plainPointLayer],
+  };
+};
+
+const herwoodCenteredShipSource: GeoJSONSourceSpecification = {
+  type: 'geojson',
+  data: 'https://meri.digitraffic.fi/api/ais/v1/locations?radius=100&latitude=61.4481&longitude=23.8521',
+};
+
+const shipLayer: CircleLayerSpecification = {
+  id: 'ships',
+  type: 'circle',
+  source: 'fintraffic-ships',
+  paint: {
+    'circle-color': '#0aa',
+    'circle-stroke-width': 2,
+  },
+};
+
+const herwoodToCapetown: GeoJSON = {
+  type: 'FeatureCollection',
+  features: [
+    {
+      type: 'Feature',
+      geometry: {
+        type: 'LineString',
+        coordinates: [
+          [23.8521, 61.4481],
+          [18.4233, -33.918861],
+        ],
+      },
+      properties: {name: 'Hervanta - Cape Town'},
+    },
+  ],
+};
+
+const herwood2CapetownSource: GeoJSONSourceSpecification = {
+  type: 'geojson',
+  data: herwoodToCapetown,
+};
+
+const herwoodLayer: LineLayerSpecification = {
+  id: 'herwood-to-capetown',
+  type: 'line',
+  source: 'herwood-to-capetown',
+  paint: {
+    'line-cap': 'round',
+    'line-width': 10,
+    'line-color': '#fa0',
+  },
+};
+
+const plainPointSource: GeoJSONSourceSpecification = {
+  type: 'geojson',
+  data: {
+    type: 'FeatureCollection',
+    features: [
+      {
+        type: 'Feature',
+        geometry: {
+          type: 'Point',
+          coordinates: [19.93481, 60.09726],
+        },
+        properties: {name: 'Mariehamn'},
+      },
+    ],
+  },
+};
+
+const plainPointLayer: CircleLayerSpecification = {
+  id: 'plain-point',
+  type: 'circle',
+  source: 'plain-point',
+  paint: {
+    'circle-color': '#0a0',
+    'circle-stroke-width': 2,
+    'circle-stroke-color': '#000',
+  },
+};
